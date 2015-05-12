216fd7682730534bf93ef23397713b0ece235ed1
Bert Spaan <bert.spaan@gmail.com>
2015-05-12 18:17:32 +0200

    uncommented bag classes

--- bag/bag.js
+++ bag/bag.js
@@ -13,82 +13,80 @@ var pitsAndRelations;
 // adres > pand
 
 var queries = [
-  // {
-  //   name: 'pand',
-  //   rowToPitsAndRelations: function(row) {
-  //     var pit = {
-  //       id: parseInt(row.id),
-  //       type: 'hg:Building',
-  //       data: {
-  //         bouwjaar: parseInt(row.bouwjaar)
-  //       },
-  //       geometry: JSON.parse(row.geometry)
-  //     };
-  //
-  //     return [
-  //       {
-  //         type: 'pits',
-  //         obj: pit
-  //       }
-  //     ];
-  //   }
-  // }
+  {
+    name: 'pand',
+    rowToPitsAndRelations: function(row) {
+      var pit = {
+        id: parseInt(row.id),
+        type: 'hg:Building',
+        data: {
+          bouwjaar: parseInt(row.bouwjaar)
+        },
+        geometry: JSON.parse(row.geometry)
+      };
 
+      return [
+        {
+          type: 'pits',
+          obj: pit
+        }
+      ];
+    }
+  },
 
+  {
+    name: 'openbareruimte',
+    rowToPitsAndRelations: function(row) {
+      var pit = {
+        id: parseInt(row.id),
+        name: row.name,
+        type: 'hg:Street',
+        data: {
+          woonplaatscode: parseInt(row.woonplaatscode)
+        }
+      };
 
-  // {
-  //   name: 'openbareruimte',
-  //   rowToPitsAndRelations: function(row) {
-  //     var pit = {
-  //       id: parseInt(row.id),
-  //       name: row.name,
-  //       type: 'hg:Street',
-  //       data: {
-  //         woonplaatscode: parseInt(row.woonplaatscode)
-  //       }
-  //     };
-  //
-  //     var relation = {
-  //       from: parseInt(row.id),
-  //       to: parseInt(row.woonplaatscode),
-  //       label: 'hg:liesIn'
-  //     };
-  //
-  //     return [
-  //       {
-  //         type: 'pits',
-  //         obj: pit
-  //       },
-  //       {
-  //         type: 'relations',
-  //         obj: relation
-  //       }
-  //     ];
-  //   }
-  // },
-  //
-  // {
-  //   name: 'woonplaats',
-  //   rowToPitsAndRelations: function(row) {
-  //     var pit = {
-  //       id: parseInt(row.id),
-  //       name: row.name,
-  //       type: 'hg:Place',
-  //       geometry: JSON.parse(row.geometry),
-  //       data: {
-  //         gemeentecode: parseInt(row.gemeentecode)
-  //       }
-  //     };
-  //
-  //     return [
-  //       {
-  //         type: 'pits',
-  //         obj: pit
-  //       }
-  //     ];
-  //   }
-  // },
-  //
+      var relation = {
+        from: parseInt(row.id),
+        to: parseInt(row.woonplaatscode),
+        label: 'hg:liesIn'
+      };
+
+      return [
+        {
+          type: 'pits',
+          obj: pit
+        },
+        {
+          type: 'relations',
+          obj: relation
+        }
+      ];
+    }
+  },
+
+  {
+    name: 'woonplaats',
+    rowToPitsAndRelations: function(row) {
+      var pit = {
+        id: parseInt(row.id),
+        name: row.name,
+        type: 'hg:Place',
+        geometry: JSON.parse(row.geometry),
+        data: {
+          gemeentecode: parseInt(row.gemeentecode)
+        }
+      };
+
+      return [
+        {
+          type: 'pits',
+          obj: pit
+        }
+      ];
+    }
+  },
+
   {
     name: 'verblijfsobject',
     rowToPitsAndRelations: function(row) {
@@ -122,16 +120,18 @@ var queries = [
         }
       ];
 
-      row.pand_ids.split(',').forEach(function(pandId) {
-        result.push({
-          type: 'relations',
-          obj: {
-            from: parseInt(row.id),
-            to: parseInt(pandId),
-            label: 'hg:liesIn'
-          }
+      if (row.pand_ids) {
+        row.pand_ids.split(',').forEach(function(pandId) {
+          result.push({
+            type: 'relations',
+            obj: {
+              from: parseInt(row.id),
+              to: parseInt(pandId),
+              label: 'hg:liesIn'
+            }
+          });
         });
-      });
+      }
 
       return result;
     }
