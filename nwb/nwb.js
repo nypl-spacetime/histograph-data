b16d2ff7cb8c79e64b336082ced8a28793ad9082
Bert Spaan <bert.spaan@gmail.com>
2015-05-19 19:29:02 +0200

    combine nwb streets to multilinestrings using leveldb

--- nwb/nwb.js
+++ nwb/nwb.js
@@ -6,6 +6,21 @@ var async = require('async');
 var es = require('event-stream');
 var AdmZip = require('adm-zip');
 var shapefile = require('shapefile');
+var levelup = require('level');
+var db = levelup(path.join(__dirname, 'db'), {
+  valueEncoding: 'json'
+});
+var reproject = require('reproject');
+var proj4 = require('proj4');
+var rd = proj4.Proj('+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs no_defs');
+var urlify = require('urlify').create({
+  addEToUmlauts: true,
+  szToSs: true,
+  toLower: true,
+  spaces: '-',
+  nonPrintable: '-',
+  trim: true
+});
 
 var baseUrl = 'http://geodata.nationaalgeoregister.nl/nwbwegen/extract/';
 var filename = 'nwbwegen.zip';
@@ -37,26 +52,27 @@ function extractRoadSegments(config, zipfile, callback) {
   });
 }
 
-exports.download = function(config, callback) {
-  request
-    .get(baseUrl + filename)
-    .pipe(fs.createWriteStream(path.join(__dirname, filename)))
-    .on('error', function(err) {
-      callback(err);
-    })
-    .on('finish', function() {
-      extractRoadSegments(config, path.join(__dirname, filename), function(err) {
+function storeRecord(record, callback) {
+  var dbKey = record.properties.WPSNAAMNEN + '-' + record.properties.STT_NAAM;
+  db.get(dbKey, function (err, value) {
+    if (err) {
+      value = {};
+      value[record.properties.WVK_ID] = record;
+
+      db.put(dbKey, value, function (err) {
         callback(err);
       });
-    });
-};
-
-exports.convert = function(config, callback) {
-  pitsAndRelations = require('../pits-and-relations')({
-    source: 'nwb',
-    truncate: true
+    } else {
+      value[record.properties.WVK_ID] = record;
+      db.put(dbKey, value, function (err) {
+        callback(err);
+      });
+    }
   });
+}
 
+
+function storeRecords(callback) {
   var reader = shapefile.reader(path.join(__dirname, shapefileName + '.shp'));
 
   reader.readHeader(function(error, header) {
@@ -71,40 +87,105 @@ exports.convert = function(config, callback) {
         if (error) return callback(error);
         if (record === shapefile.end) return callback(null);
 
-        var emit = [];
-
-        var pit = {
-          id: record.properties.WVK_ID,
-          name: record.properties.STT_NAAM,
-          type: 'hg:Street',
-          geometry: record.geometry
-        };
-
-        emit.push({
-          type: 'pits',
-          obj: pit
-        });
-
-        // TODO: add relation with place/municipality
-        // emit = emit.concat(getRelations(adminCodes, row).map(function(relation) {
-        //   return {
-        //     type: 'relations',
-        //     obj: relation
-        //   };
-        // }));
-
-        async.eachSeries(emit, function(item, callback) {
-          pitsAndRelations.emit(item.type, item.obj, function(err) {
+        storeRecord(record, function(err) {
+          if (err) {
             callback(err);
-          });
-        },
-        function(err) {
-          process.nextTick(readRecord);
+          } else {
+            process.nextTick(readRecord);
+          }
         });
-
       });
     })();
   }
+}
+
+exports.download = function(config, callback) {
+  request
+    .get(baseUrl + filename)
+    .pipe(fs.createWriteStream(path.join(__dirname, filename)))
+    .on('error', function(err) {
+      callback(err);
+    })
+    .on('finish', function() {
+      extractRoadSegments(config, path.join(__dirname, filename), function(err) {
+        if (err) {
+          callback(err);
+        }
+
+        storeRecords(function(err) {
+          callback(err);
+        });
+      });
+    });
+};
+
+exports.convert = function(config, callback) {
+  pitsAndRelations = require('../pits-and-relations')({
+    source: 'nwb',
+    truncate: true
+  });
+
+  db.createReadStream()
+    .pipe(es.map(function (data, callback) {
+      var wvkIds = Object.keys(data.value);
+
+      var id = urlify(data.key);
+      var name = data.value[wvkIds[0]].properties.STT_NAAM;
+
+      var coordinates = [];
+      wvkIds.forEach(function(wvkId) {
+        var geometry = data.value[wvkId].geometry;
+        coordinates.push(reproject.toWgs84(geometry, rd).coordinates);
+      });
+
+      var geometry = {
+        type: 'MultiLineString',
+        coordinates: coordinates
+      };
+
+      var pit = {
+        id: id,
+        name: name,
+        type: 'hg:Street',
+        data: {
+          wvk_ids: wvkIds.map(function(wvkId) { return parseInt(wvkId); })
+        },
+        geometry: geometry
+      };
+
+      var emit = [];
+
+      emit.push({
+        type: 'pits',
+        obj: pit
+      });
+
+      // TODO: add relation with place/municipality
+      // emit = emit.concat(getRelations(adminCodes, row).map(function(relation) {
+      //   return {
+      //     type: 'relations',
+      //     obj: relation
+      //   };
+      // }));
+      //
+      async.eachSeries(emit, function(item, callback) {
+        pitsAndRelations.emit(item.type, item.obj, function(err) {
+          callback(err);
+        });
+      },
+      function(err) {
+        callback(err);
+      });
+    }))
+    .on('error', function (err) {
+      callback(err)
+    })
+    .on('close', function () {
+      callback();
+    })
+    .on('end', function () {
+      callback();
+    });
 };
 
 exports.done = function(config, callback) {
