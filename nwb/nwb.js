2e236c99155e9c15aaf90bcfc8ac17f2341b81c1
Bert Spaan <bert.spaan@gmail.com>
2015-05-21 14:31:42 +0200

    removed reproject dependency, now use proj4js directly, fixes projection errors, closes #11

--- nwb/nwb.js
+++ nwb/nwb.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 var path = require('path');
-var stream = require('stream');
 var request = require('request');
 var async = require('async');
 var es = require('event-stream');
@@ -10,9 +9,9 @@ var levelup = require('level');
 var db = levelup(path.join(__dirname, 'db'), {
   valueEncoding: 'json'
 });
-var reproject = require('reproject');
 var proj4 = require('proj4');
-var rd = proj4.Proj('+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs no_defs');
+var rd = '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs';
+var wgs84 = '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees';
 var urlify = require('urlify').create({
   addEToUmlauts: true,
   szToSs: true,
@@ -33,7 +32,7 @@ function extractRoadSegments(config, zipfile, callback) {
   var zipEntries = [];
   zip.getEntries().forEach(function(zipEntry) {
     var parts = zipEntry.entryName.split('/');
-    if (parts[parts.length - 1].indexOf(shapefileName) == 0) {
+    if (parts[parts.length - 1].indexOf(shapefileName) === 0) {
       zipEntries.push({
         filename: parts[parts.length - 1],
         zipEntry: zipEntry
@@ -71,11 +70,10 @@ function storeRecord(record, callback) {
   });
 }
 
-
 function storeRecords(callback) {
   var reader = shapefile.reader(path.join(__dirname, shapefileName + '.shp'));
 
-  reader.readHeader(function(error, header) {
+  reader.readHeader(function() {
     readAllRecords(function(err) {
       callback(err);
     });
@@ -135,7 +133,10 @@ exports.convert = function(config, callback) {
       var coordinates = [];
       wvkIds.forEach(function(wvkId) {
         var geometry = data.value[wvkId].geometry;
-        coordinates.push(reproject.toWgs84(geometry, rd).coordinates);
+        var projectedCoordinates = geometry.coordinates.map(function(coordinate) {
+          return proj4(rd, wgs84, coordinate);
+        });
+        coordinates.push(projectedCoordinates);
       });
 
       var geometry = {
@@ -167,7 +168,7 @@ exports.convert = function(config, callback) {
       //     obj: relation
       //   };
       // }));
-      //
+
       async.eachSeries(emit, function(item, callback) {
         pitsAndRelations.emit(item.type, item.obj, function(err) {
           callback(err);
@@ -178,7 +179,7 @@ exports.convert = function(config, callback) {
       });
     }))
     .on('error', function (err) {
-      callback(err)
+      callback(err);
     })
     .on('close', function () {
       callback();
